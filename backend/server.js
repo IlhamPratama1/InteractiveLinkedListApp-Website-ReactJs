37b71074c79b132c352051846fbe86bd6bae9758
IlhamPratama <pratamailham206@gmail.com>
2022-06-14 01:12:01 +0700

    config

--- backend/server.js
+++ backend/server.js
@@ -41,5 +41,5 @@ require('./app/routes/index.routes')(app);
 
 // Initialize
 app.listen(8000, () => {
-    console.log(`Server is running on port ${process.env.PORT}.`);
+    console.log(`Server is running on port 8000.`);
 });
